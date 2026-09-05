9e53e66ec6b70dc5da2e4b2fba83905b841ca537
Dheerajkztr <dheerajkztr@gmail.com>
2026-09-05 23:35:40 +0530

    last changes for day1

--- Day1/pg1.py
+++ Day1/pg1.py
@@ -1,4 +1,32 @@
-name = "dheeraj K"
-age = 25
-print("My name is ",name)
-print(age)
+student_name = "Rahul"
+student_age = 20
+student_course = "Python"
+
+print("Student Name:", student_name)
+print("Student Age:", student_age)
+print("Course:", student_course)
+
+
+a = b = c = 10
+print(a)
+print(c)
+
+print(type(a))
+
+name = "Dheeraj"
+
+print("Name:", name)
+print("Length:", len(name))
+print("First character:", name[0])
+print("Last character:", name[-1])
+
+print(f"My name is {name}")
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+future_age = age + 5
+
+print(f"Hello {name}!")
+print(f"You are currently {age} years old.")
+print(f"In 5 years, you will be {future_age} years old.")
